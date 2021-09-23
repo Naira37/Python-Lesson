628e2a7c4690588999ee877d695609c1ad061c3a
Naira37 <nair.balta90@gmail.com>
2021-09-23 20:05:44 +0400

    my first commit

---
+++ lesson7.py
@@ -0,0 +1,9 @@
+# first = input('5 + 5 ') == '10'
+# second = input('6 + 6 ') == '12' 
+# res = first and second
+# print(res)
+
+number = int(input('number '))
+
+res = number + int(2 * str(number))+ int(3 * str(number))
+print(res)
