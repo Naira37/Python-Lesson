b00848e9e1536246e04322cb724fc2424c7b0a46
Naira37 <nair.balta90@gmail.com>
2021-09-23 20:04:53 +0400

    my first commit

---
+++ lesson8.py
@@ -0,0 +1,113 @@
+# a = 34 
+# b = 14
+# if b>a:
+# 	print('b is big')
+# else:
+# 	print('a is big')
+
+# age1 = int(input('Age 1 '))
+# age2 = int(input('Age 2 '))
+# age3 = int(input('Age 3 '))
+# if age2>age3 and age2>age1:
+# 	print('Age 2 is big')
+# elif age1>age2 and age1>age3:
+# 	print('Age 1 is big')
+# else:
+# 	print('Age 3 is big')
+
+
+# num = float(input("Enter a number: "))
+# if num >=0:
+# 	if num == 0:
+# 		print("Zero")
+# 	else:
+# 		print("Positive Number")
+# else:
+# 	print("Negative Number")
+
+
+
+# res = None
+# if res is True:
+# 	print("Do you think None is True? ")
+# elif res is False:
+# 	print("Do you think None is False? ")
+# else:
+# 	print("None is not True, or False, None is just None...")
+
+# word = input('some word ')
+# if word.isalpha():
+# 	print('all leter')
+# else:
+# 	print('You have a number')
+
+
+
+# word = input('some word ')
+# if word.isdigit():
+# 	print('all Digit')
+# else:
+# 	print('You have a letter')
+
+
+
+# word = input('some word ')
+# if word.isalnum():
+# 	print('all Digit an letter')
+# else:
+# 	print('You have a chars')
+
+
+# word = input('some word ')
+# if word.istitle():
+# 	print('all first upper')
+# else:
+# 	print('lower')
+
+
+
+# word = input('some word ')
+# if word.isupper():
+# 	print('all upper')
+# else:
+# 	print('You have a lower')
+
+
+# word = input('some word ')
+# if word.islower():
+# 	print('all lower')
+# else:
+# 	print('You have a upper')
+
+
+# word = input('some word ')
+# word = word.capitalize()
+# print(word)
+
+# dog_age = int(input('Dog age '))
+# human_age = ''
+# if dog_age<0:
+# 	print('Wrong')
+# elif dog_age >=0 and dog_age <=2:
+# 	human_age = dog_age * 5.25
+# else:
+# 	human_age = 10.5 + (dog_age-2) * 4
+# print(human_age)
+
+
+vowel = 'auoiey '
+letter = input('Letter ')
+if letter in vowel:
+	print('Vowel')
+else:
+	print('Consonant')
+
+	
+
+
+
+
+
+
+
+
