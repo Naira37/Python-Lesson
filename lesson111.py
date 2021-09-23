910d12911b57f80d64eb53f6c90d738edb06df0f
Naira37 <nair.balta90@gmail.com>
2021-09-23 19:41:15 +0400

    my first commit

---
+++ lesson111.py
@@ -0,0 +1,35 @@
+# name = input('What is your name? ')
+# print('Hi ' + name)
+
+# name = input('What is your name? ')
+# favourite_colour = input('What is your favourite colour? ') 
+# print(name + ' likes ' + favourite_colour)
+
+# birth_year = input('Birth year: ')
+# age = 2021 - int(birth_year)
+# print(age)
+
+
+# weight_lbs = input('Weight (lbs): ')
+# weight_kg = int(weight_lbs) * 0.45
+# print(weight_kg)
+
+
+# course = 'Pithon Course for "Beginners"'
+# print(course[-2])
+course ='Pithon is for Beginners'
+print(course[-9])
+
+course = 'Pithon is for Beginners'
+print(course[:])
+
+name = 'Jennifer'
+print(name[1:-1])
+
+course ='Pithon for Beginners'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course)
+print(course.find('P'))
+print(course.replace('Beginners', 'Absolute Beginners'))
